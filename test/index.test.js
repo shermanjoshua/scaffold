0e8ab26db9faf634730461a8043ca579b7a66ce5
Josh Sherman <sherman.joshua@gmail.com>
2018-11-26 11:53:57 -0700

    Updated tests

--- test/index.test.js
+++ test/index.test.js
@@ -2,13 +2,13 @@ import helloThere from '../src/index';
 
 describe('Sample tests', () => {
   it('Should be a string', () => {
-    const foo = 'foo';
+    const foo = helloThere();
     foo.should.be.a('string');
   });
 
   it('Should be 3 characters long', () => {
-    const foo = 'foo';
-    foo.should.have.lengthOf(3);
+    const foo = helloThere();
+    foo.should.have.lengthOf(5);
   });
 
   it('Should have correct message', () => {
