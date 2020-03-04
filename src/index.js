d34304c848e40accae4656883d47673ff2f03ffc
Josh Sherman <sherman.joshua@gmail.com>
2020-03-04 11:47:38 -0700

    updated deps

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ export function helloThere() {
   return 'Hello';
 }
 
-const bar = [1, 1, 1, 1, 1];
-
 export function reduced() {
-  let value = bar.reduce((acc, value) => acc + value, 0);
+  const list = [1, 1, 1, 1, 1];
+  const value = list.reduce((acc, val) => acc + val, 0);
   return value;
 }
