4025346d88980b439b4a24fee9d5a2ef2710a9c0
Josh Sherman <sherman.joshua@gmail.com>
2020-03-04 11:43:57 -0700

    added test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-export default function helloThere() {
+export function helloThere() {
   return 'Hello';
+}
+
+const bar = [1, 1, 1, 1, 1];
+
+export function reduced() {
+  let value = bar.reduce((acc, value) => acc + value, 0);
+  return value;
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,18 +1,23 @@
-import helloThere from '../src/index';
+import * as helpers from '../src/index';
 
 describe('Sample tests', () => {
   it('Should be a string', () => {
-    const foo = helloThere();
+    const foo = helpers.helloThere();
     foo.should.be.a('string');
   });
 
   it('Should be 3 characters long', () => {
-    const foo = helloThere();
+    const foo = helpers.helloThere();
     foo.should.have.lengthOf(5);
   });
 
   it('Should have correct message', () => {
-    const message = helloThere();
+    const message = helpers.helloThere();
     message.should.equal('Hello');
+  });
+
+  it('Should be 5', () => {
+    const list = helpers.reduced();
+    list.should.equal(5);
   });
 });
