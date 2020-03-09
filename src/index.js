e1233d318d8c4903cd74527ac2792f65e0c86ee5
Josh Sherman <sherman.joshua@gmail.com>
2020-03-09 12:33:33 -0600

    map

--- src/index.js
+++ src/index.js
@@ -7,3 +7,9 @@ export function reduced() {
   const value = list.reduce((acc, val) => acc + val, 0);
   return value;
 }
+
+export function mapped() {
+  const list = [1, 2, 3, 4, 5];
+  const value = list.map((val) => val * 10);
+  return value;
+}
--- test/index.test.js
+++ test/index.test.js
@@ -15,9 +15,18 @@ describe('Sample tests', () => {
     const message = helpers.helloThere();
     message.should.equal('Hello');
   });
+});
 
+describe('Reduce Tests', () => {
   it('Should be 5', () => {
     const list = helpers.reduced();
     list.should.equal(5);
+  });
+});
+
+describe('Map Tests', () => {
+  it('Should be 10', () => {
+    const list = helpers.mapped();
+    list.should.deep.equal([10, 20, 30, 40, 50]);
   });
 });
